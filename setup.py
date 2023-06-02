baabec7764ae817ad89f769dd3ddc792bd9e8e0e
Benjamin Bolte <ben@bolte.cc>
2023-06-01 23:46:39 -0400

    version string

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 with open("pretrained/__init__.py", "r", encoding="utf-8") as fh:
     version_re = re.search(r"^__version__ = \"([^\"]*)\"", fh.read(), re.MULTILINE)
-assert version_re is not None, "Could not find version in ml/__init__.py"
+assert version_re is not None, "Could not find version in pretrained/__init__.py"
 version: str = version_re.group(1)
 
 
