502edc8413168e0572168bc428f4afeadb42ea37
Ben Bolte <ben@bolte.cc>
2023-06-07 00:19:25 -0400

    catch rwkv errors (#14)

--- pretrained/rwkv.py
+++ pretrained/rwkv.py
@@ -363,15 +363,20 @@ class RwkvPredictor:
         self.model = rwkv_model
 
     def sample_probs(self, probs: Tensor, temperature: float = 1.0, top_p: float = 0.85) -> Tensor:
-        probs = probs ** (1 / temperature)
-        probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
-        probs_sum = torch.cumsum(probs_sort, dim=-1)
-        mask = probs_sum - probs_sort > top_p
-        probs_sort[mask] = 0.0
-        probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
-        next_token = torch.multinomial(probs_sort.squeeze(-3), num_samples=1)
-        next_token = torch.gather(probs_idx, -1, next_token[..., None, :, :]).squeeze(-1)
-        return next_token
+        try:
+            probs = probs ** (1 / temperature)
+            probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
+            probs_sum = torch.cumsum(probs_sort, dim=-1)
+            mask = probs_sum - probs_sort > top_p
+            probs_sort[mask] = 0.0
+            probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True) + 1e-6)
+            next_token = torch.multinomial(probs_sort.squeeze(-3), num_samples=1)
+            next_token = torch.gather(probs_idx, -1, next_token[..., None, :, :]).squeeze(-1)
+            return next_token
+
+        except Exception:
+            logger.exception("Error sampling from probabilities.")
+            return probs.new_zeros(probs.shape[:-1], dtype=torch.long)
 
     @torch.no_grad()
     def generate(
