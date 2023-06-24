508db8086cf07e846abebccdd8742c9b86bb992c
Ben Bolte <ben@bolte.cc>
2023-06-24 19:36:52 -0400

    freeze options (#38)
    * freeze options
    
    * fix lint
    
    * more lint
    
    * more lint

--- pretrained/rwkv.py
+++ pretrained/rwkv.py
@@ -542,36 +542,57 @@ def get_wkv_fn_cuda(key: WkvFnKey) -> Callable[[Tensor, Tensor, Tensor, Tensor, 
             raise ValueError(f"Unsupported key: {key}")
 
 
+def get_default_wkv_fn_key() -> WkvFnKey:
+    if "WKV_FN" in os.environ:
+        assert (wkv_fn_str := os.environ["WKV_FN"]) in get_args(WkvFnKey), f"Unsupported WKV_FN: {wkv_fn_str}"
+        return cast(WkvFnKey, wkv_fn_str)
+
+    warnings.warn("Using default WKV_FN: eps")
+    return "eps"
+
+
 class Attention(nn.Module):
     init_x: Tensor
     init_state: Tensor
 
     def __init__(
         self,
-        emb_dim: int,
+        dim: int,
         lora_rank: int | None = None,
         lora_alpha: float = 1.0,
         lora_dropout: float = 0.0,
-        wkv_key: WkvFnKey = "log",
+        freeze: bool = False,
+        wkv_key: WkvFnKey | None = None,
     ) -> None:
         super().__init__()
 
-        self.time_decay = nn.Parameter(torch.empty(emb_dim))
-        self.time_first = nn.Parameter(torch.empty(emb_dim))
+        self.time_decay = nn.Parameter(torch.empty(dim))
+        self.time_first = nn.Parameter(torch.empty(dim))
 
-        self.time_mix_k = nn.Parameter(torch.empty(1, 1, emb_dim))
-        self.time_mix_v = nn.Parameter(torch.empty(1, 1, emb_dim))
-        self.time_mix_r = nn.Parameter(torch.empty(1, 1, emb_dim))
+        self.time_mix_k = nn.Parameter(torch.empty(1, 1, dim))
+        self.time_mix_v = nn.Parameter(torch.empty(1, 1, dim))
+        self.time_mix_r = nn.Parameter(torch.empty(1, 1, dim))
 
-        self.key = maybe_lora(nn.Linear(emb_dim, emb_dim, bias=False), lora_rank, lora_alpha, lora_dropout)
-        self.value = maybe_lora(nn.Linear(emb_dim, emb_dim, bias=False), lora_rank, lora_alpha, lora_dropout)
-        self.receptance = maybe_lora(nn.Linear(emb_dim, emb_dim, bias=False), lora_rank, lora_alpha, lora_dropout)
-        self.output = maybe_lora(nn.Linear(emb_dim, emb_dim, bias=False), lora_rank, lora_alpha, lora_dropout)
+        if freeze:
+            self.time_decay.requires_grad_(False)
+            self.time_first.requires_grad_(False)
+            self.time_mix_k.requires_grad_(False)
+            self.time_mix_v.requires_grad_(False)
+            self.time_mix_r.requires_grad_(False)
+
+        self.key = maybe_lora(nn.Linear(dim, dim, False), lora_rank, lora_alpha, lora_dropout, freeze=freeze)
+        self.value = maybe_lora(nn.Linear(dim, dim, False), lora_rank, lora_alpha, lora_dropout, freeze=freeze)
+        self.receptance = maybe_lora(nn.Linear(dim, dim, False), lora_rank, lora_alpha, lora_dropout, freeze=freeze)
+        self.output = maybe_lora(nn.Linear(dim, dim, False), lora_rank, lora_alpha, lora_dropout, freeze=freeze)
+
+        if wkv_key is None:
+            wkv_key = get_default_wkv_fn_key()
 
         self.wkv_fn = get_wkv_fn(wkv_key)
         self.wkv_fn_cuda = get_wkv_fn_cuda(wkv_key)
-        self.register_buffer("init_x", torch.zeros(1, 1, emb_dim), persistent=False)
-        self.register_buffer("init_state", initial_state_with_eps(emb_dim), persistent=False)
+
+        self.register_buffer("init_x", torch.zeros(1, 1, dim), persistent=False)
+        self.register_buffer("init_state", initial_state_with_eps(dim), persistent=False)
 
     def time_shift(self, last_x: Tensor, x: Tensor) -> Tensor:
         _, tsz, _ = x.shape
@@ -608,22 +629,27 @@ class FeedForward(nn.Module):
 
     def __init__(
         self,
-        emb_dim: int,
+        dim: int,
         ffn_dim: int,
         lora_rank: int | None = None,
         lora_alpha: float = 1.0,
         lora_dropout: float = 0.0,
+        freeze: bool = False,
     ) -> None:
         super().__init__()
 
-        self.time_mix_k = nn.Parameter(torch.empty(1, 1, emb_dim))
-        self.time_mix_r = nn.Parameter(torch.empty(1, 1, emb_dim))
+        self.time_mix_k = nn.Parameter(torch.empty(1, 1, dim))
+        self.time_mix_r = nn.Parameter(torch.empty(1, 1, dim))
 
-        self.key = maybe_lora(nn.Linear(emb_dim, ffn_dim, bias=False), lora_rank, lora_alpha, lora_dropout)
-        self.receptance = maybe_lora(nn.Linear(emb_dim, emb_dim, bias=False), lora_rank, lora_alpha, lora_dropout)
-        self.value = maybe_lora(nn.Linear(ffn_dim, emb_dim, bias=False), lora_rank, lora_alpha, lora_dropout)
+        if freeze:
+            self.time_mix_k.requires_grad_(False)
+            self.time_mix_r.requires_grad_(False)
 
-        self.register_buffer("init_state", torch.zeros(1, 1, emb_dim), persistent=False)
+        self.key = maybe_lora(nn.Linear(dim, ffn_dim, False), lora_rank, lora_alpha, lora_dropout, freeze=freeze)
+        self.receptance = maybe_lora(nn.Linear(dim, dim, False), lora_rank, lora_alpha, lora_dropout, freeze=freeze)
+        self.value = maybe_lora(nn.Linear(ffn_dim, dim, False), lora_rank, lora_alpha, lora_dropout, freeze=freeze)
+
+        self.register_buffer("init_state", torch.zeros(1, 1, dim), persistent=False)
 
     def time_shift(self, last_x: Tensor, x: Tensor) -> Tensor:
         _, tsz, _ = x.shape
@@ -653,7 +679,10 @@ class Block(nn.Module):
         lora_dropout: float = 0.0,
         lora_attn: bool = True,
         lora_ffn: bool = True,
-        wkv_key: WkvFnKey = "log",
+        freeze_layer_norm: bool = False,
+        freeze_attn: bool = False,
+        freeze_ffn: bool = False,
+        wkv_key: WkvFnKey | None = None,
     ) -> None:
         super().__init__()
 
@@ -661,11 +690,18 @@ class Block(nn.Module):
         self.ln1 = nn.LayerNorm(emb_dim)
         self.ln2 = nn.LayerNorm(emb_dim)
 
+        if freeze_layer_norm:
+            if self.ln0 is not None:
+                self.ln0.requires_grad_(False)
+            self.ln1.requires_grad_(False)
+            self.ln2.requires_grad_(False)
+
         self.att = Attention(
             emb_dim,
             lora_rank=lora_rank if lora_attn else None,
             lora_alpha=lora_alpha,
             lora_dropout=lora_dropout,
+            freeze=freeze_attn,
             wkv_key=wkv_key,
         )
 
@@ -675,6 +711,7 @@ class Block(nn.Module):
             lora_rank=lora_rank if lora_ffn else None,
             lora_alpha=lora_alpha,
             lora_dropout=lora_dropout,
+            freeze=freeze_ffn,
         )
 
     def forward(self, x: Tensor, state: State | None = None) -> tuple[Tensor, State]:
@@ -701,9 +738,22 @@ class Rwkv(nn.Module):
         lora_top_k_blocks: int | None = None,
         lora_attn: bool = True,
         lora_ffn: bool = True,
-        wkv_key: WkvFnKey = "log",
+        freeze_non_lora: bool = False,
+        freeze_layer_norm: bool | None = None,
+        freeze_attn: bool | None = None,
+        freeze_ffn: bool | None = None,
+        wkv_key: WkvFnKey | None = None,
     ) -> None:
         super().__init__()
+
+        if lora_rank is None:
+            freeze_non_lora = False
+        if freeze_layer_norm is None:
+            freeze_layer_norm = freeze_non_lora
+        if freeze_attn is None:
+            freeze_attn = freeze_non_lora
+        if freeze_ffn is None:
+            freeze_ffn = freeze_non_lora
 
         if lora_top_k_blocks is None:
             min_block = 0
@@ -715,6 +765,7 @@ class Rwkv(nn.Module):
             lora_rank if lora_embeddings else None,
             lora_alpha,
             lora_dropout,
+            freeze=freeze_non_lora,
         )
         blocks = [
             Block(
@@ -725,17 +776,23 @@ class Rwkv(nn.Module):
                 lora_dropout=lora_dropout,
                 lora_attn=lora_attn,
                 lora_ffn=lora_ffn,
+                freeze_layer_norm=freeze_layer_norm,
+                freeze_attn=freeze_attn,
+                freeze_ffn=freeze_ffn,
                 wkv_key=wkv_key,
             )
             for i in range(num_layers)
         ]
         self.blocks = nn.ModuleList(blocks)
         self.ln_out = nn.LayerNorm(emb_dim)
+        if freeze_layer_norm:
+            self.ln_out.requires_grad_(False)
         self.head = maybe_lora(
             nn.Linear(emb_dim, num_tokens, bias=False),
             lora_rank if lora_linear else None,
             lora_alpha,
             lora_dropout,
+            freeze=freeze_non_lora,
         )
 
     def tensor_to(self, x: Tensor) -> Tensor:
@@ -846,8 +903,12 @@ def pretrained_rwkv(
     lora_top_k_blocks: int | None = None,
     lora_attn: bool = True,
     lora_ffn: bool = True,
+    freeze_non_lora: bool = False,
+    freeze_layer_norm: bool | None = None,
+    freeze_attn: bool | None = None,
+    freeze_ffn: bool | None = None,
     empty: bool = False,
-    wkv_key: WkvFnKey = "log",
+    wkv_key: WkvFnKey | None = None,
 ) -> Rwkv:
     """Returns a pretrained RWKV model.
 
@@ -864,6 +925,11 @@ def pretrained_rwkv(
             decomposition.
         lora_attn: Whether to use LoRA for the attention layers.
         lora_ffn: Whether to use LoRA for the feed-forward layers.
+        freeze_non_lora: Whether to freeze the non-LoRA parameters. This value
+            will override the other freeze parameters if they are None.
+        freeze_layer_norm: Whether to freeze the layer normalization parameters.
+        freeze_attn: Whether to freeze the attention parameters.
+        freeze_ffn: Whether to freeze the feed-forward parameters.
         empty: Whether to return an empty model with the same structure as the
             pretrained model.
         wkv_key: The choice of WKV function to use. They are mathematically
@@ -889,11 +955,15 @@ def pretrained_rwkv(
             lora_top_k_blocks=lora_top_k_blocks,
             lora_attn=lora_attn,
             lora_ffn=lora_ffn,
+            freeze_non_lora=freeze_non_lora,
+            freeze_layer_norm=freeze_layer_norm,
+            freeze_attn=freeze_attn,
+            freeze_ffn=freeze_ffn,
             wkv_key=wkv_key,
         )
 
     if empty:
-        model._apply(meta_to_empty_func(device.get_device(), torch.half))
+        model._apply(meta_to_empty_func(device.get_device(), torch.bfloat16))
         model._apply(lambda x: device.tensor_to(x))
         return model
 
@@ -905,7 +975,7 @@ def pretrained_rwkv(
 
     # Build the transformer and loads the checkpoint.
     with Timer("loading state dict", spinner=True):
-        model._apply(meta_to_empty_func(device.get_device(), torch.half))
+        model._apply(meta_to_empty_func(device.get_device(), torch.bfloat16))
         model.load_state_dict(ckpt)
         model._apply(lambda x: device.tensor_to(x))
 
